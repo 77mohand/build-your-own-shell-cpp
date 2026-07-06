24fa2e8cdbda4876f90dccb3bf11e62e281e3192
mohand <mohandpro50@gmail.com>
2026-07-06 21:42:59 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <cstdlib>
+#include <filesystem>
+#include <sstream>
 using namespace std;
+namespace fs = std::filesystem;
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -15,19 +20,51 @@ int main() {
     getline(cin, input);
     if(input == "exit") {
       break;
-    } else if (input.substr(0, 5) == "echo "){
-      cout<<input.substr(5)<<endl;
-    } else if ( input.substr(0, 5)== "type "){
+    }
 
-      if (input.substr(5) == "type" || input.substr(5) == "exit" || input.substr(5) == "echo" ) {
-      cout<< input.substr(5) << " is a shell builtin "<<endl;
-      }else {
-      cout << input.substr(5) << ": not found" << endl;
-      }
+    else if (input.substr(0, 5) == "echo "){
+      cout<<input.substr(5)<<endl;
+    }
+
+    else if ( input.substr(0, 5)== "type "){
+
+        if (input.substr(5) == "type" || input.substr(5) == "exit" || input.substr(5) == "echo" ) {
+          cout<< input.substr(5) << " is a shell builtin "<<endl;
+        }else {
+
+          char *path_env = getenv("PATH");
+
+          if (path_env != nullptr && *path_env != '\0') {
+            string full_path = path_env;
+            replace(full_path.begin(), full_path.end(), ';', ':');
+            stringstream ss(full_path);
+            string folder_path;
+            bool found = false;
+
+            while (getline(ss, folder_path, ':')) {
+
+              fs::path folder_path1 = folder_path + "/" + input.substr(5);
+
+              if (fs::exists(folder_path1)) {
+                found = true;
+                cout << input.substr(5) << " is " << folder_path + "/" + input.substr(5) << endl;
+                break;
+              }
+            }
+            if (!found) {
+                cout << input.substr(5) << ": not found" << endl;
+            }
+          }else {
+            cout<< "PATH environment variable is not set." << endl;
+          }
+
+        }
+
     }
 
     else{
       cout << input<< ": command not found" << endl;
     }
+
   }
 }
