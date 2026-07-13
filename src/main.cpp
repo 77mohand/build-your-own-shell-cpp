9be97e39f3a8f846592391599f0c141037c6b21e
Mohand Amr <mohandpro50@gmail.com>
2026-07-13 14:47:31 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ int main() {
 
     else if ( input.substr(0, 5)== "type "){
 
-        if (input.substr(5) == "type" || input.substr(5) == "exit" || input.substr(5) == "echo" ) {
+        if (input.substr(5) == "type" || input.substr(5) == "exit" || input.substr(5) == "echo" || input.substr(5) == "pwd") {
           cout<< input.substr(5) << " is a shell builtin "<<endl;
         }else {
 
