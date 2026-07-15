0a1c6d33b1ef4a50e6c51f3b80c1d89bd19eef3c
Mohand Amr <mohandpro50@gmail.com>
2026-07-15 19:54:28 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -38,13 +38,15 @@ vector<string>  parse_input(const string& input) {
 
     if (input[i] == ' '){
       if (!in_single_quotes && !in_double_quotes){
-        tokens.push_back(conten);
-        conten = "";
-      }
+        if (!conten.empty()){
+          tokens.push_back(conten);
+          conten = "";
+        }
       continue;
+      }
     }
 
-    conten += input[i];
+    conten += current;
 
   }
 
